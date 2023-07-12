34a92f108fcf50950fa8e67e25405a827693c9c6
Vasya <khitrik.rozanova.svetlana@gmail.com>
2023-07-12 05:46:04 +0300

    feat: add main

--- src/main/java/org/itmo/Main.java
+++ src/main/java/org/itmo/Main.java
@@ -1,7 +1,36 @@
 package org.itmo;
 
+import static org.itmo.constants.Commands.*;
+
+import java.io.IOException;
+import org.itmo.game.Controller;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        if(args.length < 2) {
+            System.out.println("Not enough arguments in the command");
+        } else {
+            if (args[0].equals(BASE_COMMAND)) {
+                switch (args[1]) {
+                    case PLAY -> {
+                        new Controller().play();
+                    }
+                    case LOAD_LEVEL -> {
+                    
+                    }
+                    case GENERATE_LEVEL -> {
+                    
+                    }
+                    case SOLVE_LEVEL -> {
+                    
+                    }
+                    default -> {
+                        System.out.println("Unknown command");
+                    }
+                }
+            } else {
+                System.out.println("The error command must begin with the word \"sokoban\"");
+            }
+        }
     }
 }
